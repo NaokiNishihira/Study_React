5d8f98459b8f1f756115087b44f3cf3e1192e56e
NaokiNishihira <dadakko1206@gmail.com>
2024-05-14 20:18:36 +0900

    change:UseState_cynchro

--- Src_React/react-app/src/UseState.jsx
+++ Src_React/react-app/src/UseState.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import {useState} from 'react';
 
 export default function UseState(){
+    const [count, setCount] = useState(0);
+       function handleClick(){
+        setCount(count+1);
+    }
+    
     function MyButton(){
-        const [count, setCount] = useState(0);
-
-        function handleClick(){
-            setCount(count+1);
-        }
-
         return(
             <button onClick={handleClick}>
                 Clicked {count} times
