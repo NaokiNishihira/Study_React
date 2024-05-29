33ad38443733900f7686311d31e7b1b85b8bdc8e
NaokiNishihira <dadakko1206@gmail.com>
2024-05-29 16:52:43 +0900

    remove:LinkApp LinkHome LinkTopPage

--- Src_React/react-app/src/index.js
+++ Src_React/react-app/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import { AppRoutes } from './AppRoutes';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import AppLink from './AppLink';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
