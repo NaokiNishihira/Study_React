1bfc5540d158e5dc2042464af97faf90fb423bb1
NaokiNishihira <dadakko1206@gmail.com>
2024-06-05 23:11:33 +0900

    fix:AppHome

--- Src_React/react-app/src/App/AppHome.jsx
+++ Src_React/react-app/src/App/AppHome.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import {useState} from 'react';
-import {Link} from 'react-router-dom';
-import UseCss from './UseCss.css'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './UseCss.css';
 
-export default function AppHome(){
+export default function AppHome() {
     const [text, setText] = useState('名前入力');
-    const ButtonState = {name: '入力完了'};
+    const ButtonState = { name: '入力完了' };
     const message = "Hello from Home Page!";
 
-    return(
+    return (
         <div>
             <h1 className="title">Welcome</h1>
-            <h2 className='title'>優しいSNS</h2>
+            <h2 className="title">優しいSNS</h2>
             <br />
-            <Link to="/TopPage">
-                <div className='container'>
-                    <button>{'新着'}</button>
-                    <button>{'アニメ'}</button>
-                    <button>{'スポーツ'}</button>
-                </div>
-            </Link>
+            <div className="textbox-container">
+                <Link to="/TopPage">
+                    <div className="container">
+                        <button>{'新着'}</button>
+                        <button>{'アニメ'}</button>
+                        <button>{'スポーツ'}</button>
+                    </div>
+                </Link>
+            </div>
+            <div className="textbox">
+                <input type="text" className="textbox_size" name="name" />
+            </div>
         </div>
     );
 }
