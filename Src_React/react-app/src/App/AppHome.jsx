480fe3b74abbeb46aa0535baf44603db0b25da36
NaokiNishihira <dadakko1206@gmail.com>
2024-06-16 17:21:44 +0900

    fix:AppHome and UseCss add:SearchIcon

--- Src_React/react-app/src/App/AppHome.jsx
+++ Src_React/react-app/src/App/AppHome.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './UseCss.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function AppHome() {
     const [text, setText] = useState('名前入力');
     const ButtonState = { name: '入力完了' };
-    const message = "Hello from Home Page!";
+
+    const handleSearch = () => {
+        alert(`検索クエリ: ${text}`);
+    };
 
     return (
         <div>
@@ -23,7 +27,15 @@ export default function AppHome() {
                 </Link>
             </div>
             <div className="textbox">
-                <input type="text" className="textbox_size" name="name" />
+                <div className="textbox-wrapper">
+                    <input
+                        type="text"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        className="textbox_size"
+                    />
+                    <FontAwesomeIcon icon={faSearch} className="search-icon" onClick={handleSearch} />
+                </div>
             </div>
         </div>
     );
