0a6a53ee9bbc108dece8e8efb235a32551e62091
NaokiNishihira <dadakko1206@gmail.com>
2024-06-21 19:06:24 +0900

    fix:AppHome genre and genretitle

--- Src_React/react-app/src/App/AppHome.jsx
+++ Src_React/react-app/src/App/AppHome.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 import './UseCss.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Table from './Table';
 
 export default function AppHome() {
-    const [text, setText] = useState('名前入力');
-    const ButtonState = { name: '入力完了' };
+    const [text, setText] = useState('');       //テキストボックスの中身を格納する変数
+    const GenresName = {1:'トレンド', 2:'新着', 3:'アニメ', 4:'スポーツ', 5:'ニュース'};      //ジャンルを指定するための変数
 
     const handleSearch = () => {
         alert(`検索クエリ: ${text}`);
@@ -18,11 +19,13 @@ export default function AppHome() {
             <h2 className="title">優しいSNS</h2>
             <br />
             <div className="textbox-container">
+                {/* ボタンを押すとページが飛ぶようになっている */}
                 <Link to="/TopPage">
                     <div className="container">
-                        <button>{'新着'}</button>
-                        <button>{'アニメ'}</button>
-                        <button>{'スポーツ'}</button>
+                        <button>{GenresName[2]}</button>
+                        <button>{GenresName[3]}</button>
+                        <button>{GenresName[4]}</button>
+                        <button>{GenresName[5]}</button>
                     </div>
                 </Link>
             </div>
@@ -33,10 +36,15 @@ export default function AppHome() {
                         value={text}
                         onChange={(e) => setText(e.target.value)}
                         className="textbox_size"
+                        placeholder="Search"
                     />
                     <FontAwesomeIcon icon={faSearch} className="search-icon" onClick={handleSearch} />
                 </div>
             </div>
+            <div>
+                <h3 className="title title-special">{GenresName[1]}</h3>
+            </div>
+            <Table />
         </div>
     );
 }
